3ec266fb7752307602acdb7ca785091e0df27dc7
Rstar1998 <rosscarvalho22@gmail.com>
2020-05-26 19:00:38 +0530

    make it better

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def hello_world():
 	        'rank' : td_list[0].find_all('b')[0].getText(),
 	        'flaglink' : td_list[1].find_all('img')[0].get('src'),
 	        'name' : td_list[2].find_all('a')[0].getText(),
-	        'value' : td_list[3].find_all('span')[0].getText()
+	        'value' : td_list[3].find_all('span')[0].getText(),
+	        'href' : td_list[2].find_all('a')[0].get('href')
 	    })
 	     
 	resp['data']=data_list
